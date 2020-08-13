5daae4baff31f2a7f6c25181cd64beb949520e91
ojh <awfawf123@naver.com>
2020-08-13 17:51:03 +0900

    8/13

--- firstProject/src/com/yedam/exam/Exercise009.java
+++ firstProject/src/com/yedam/exam/Exercise009.java
@@ -1,5 +1,28 @@
 package com.yedam.exam;
 
+import java.utill.Scanner;
+
 public class Exercise009 {
+	public static void main(String[] args) {
+		boolean run = true;
+		int studentNum =0;
+		int[] scores = null;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(run) {
+			System.out.println("--------------------------");
+			System.out.println("1.학생수2.점수입력3.리스트4.분석5.종료");
+			System.out.println("--------------------------");
+			System.out.println("선택> ");
+			int selectNo = scn.nextInt();
+			if (selectNo == 1) {
+				System.out.print("학생수 입력: ");
+				studentNum = scn.nextInt();
+				scores = new int[studentNum]; // new int[5];
+			} else if(selectNo ==2) {
+				
+			}else if(selectNo ==2)
+		}
+	}
 
 }
